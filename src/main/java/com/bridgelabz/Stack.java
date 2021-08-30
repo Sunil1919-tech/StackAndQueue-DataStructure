2a5ef8af8ff3eb47718004f43146025bb5b14387
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-30 17:47:58 +0530

    Added: Create Stack using linked list

---
+++ src/main/java/com/bridgelabz/Stack.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+/*
+Stack class is to implement the stack Operation
+ */
+public class Stack<T> {
+    MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+
+    // push the Node to stack
+    public void push(INode newNode) {
+        myLinkedList.add(newNode);
+    }
+
+
+    //peek the top node in stack
+    public INode peek() {
+        return myLinkedList.head;
+    }
+}
---
+++ src/test/java/com/bridgelabz/MyLinkedListTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+/**
+ * purpose-to create stack and Queue Using Linked List
+ * @author Sunil
+ * @version 16.0
+ * @since 25/08/2021
+ */
+public class MyLinkedListTest {
+    //testcase to Create the Stack Using Linked list
+    @Test
+    public void IfStackIsCreated_shouldReturnTrue() {
+        Stack<MyNode<Integer>> stack = new Stack<>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        stack.push(myThirdNode);
+        stack.push(mySecondNode);
+        stack.push(myFirstNode);
+        INode peek = stack.peek();
+        Assertions.assertEquals(peek, myFirstNode);
+    }
+}
